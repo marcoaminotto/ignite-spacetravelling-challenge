c50f807e4128a25c57f18edae5addc31fadc2d34
Marco Echevestre <devminotto@gmail.com>
2021-08-26 14:02:59 +0200

    Implement header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
-export default function Header() {
-  // TODO
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.headerContainer}>
+      <div>
+        <a href="/">
+          <img src="/images/logo.svg" alt="logo" />
+        </a>
+      </div>
+    </header>
+  );
 }
