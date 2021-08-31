83ef623c175d8fd5230b0d49f70b9636a440c59e
Marco Echevestre <devminotto@gmail.com>
2021-08-31 12:57:21 +0200

    Implement element link from next.js

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,15 @@
+import Link from 'next/link';
 import styles from './header.module.scss';
 
 export default function Header(): JSX.Element {
   return (
     <header className={styles.headerContainer}>
       <div>
-        <a href="/">
-          <img src="/images/logo.svg" alt="logo" />
-        </a>
+        <Link href="/">
+          <a>
+            <img src="/images/logo.svg" alt="logo" />
+          </a>
+        </Link>
       </div>
     </header>
   );
